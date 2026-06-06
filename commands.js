7265e66968a99eb9bfe92af84bdb0f5c37f5d16d
grff-arch <andre.kiseleov2019@gmail.com>
2026-06-06 14:37:39 +0400

    Test for Linux

--- commands.js
+++ commands.js
@@ -5,7 +5,6 @@ const { exec } = require('child_process');
 const PLATFORM = os.platform();
 
 const nircmd = path.join(__dirname, "bin", "nircmd.exe")
-
 const nircmdFolder = path.join(__dirname, "bin")
 
 const winCommands = {
@@ -18,18 +17,41 @@ const winCommands = {
     'sleep': `"${nircmd}" standby`
 };
 
-const handlerCom = (command) => {
-    const systemCom = winCommands[command];
+const linuxCommands = {
+    'up': `wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+`,
+    'mute_on': `wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle`,
+    'mute_off': `wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle`,
+    'down': `wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-0`,
+    'monitor_off': `busctl --user call org.gnome.Mutter.DisplayConfig /org/gnome/Mutter/DisplayConfig org.gnome.Mutter.DisplayConfig PowerSaveMode i 1`,
+    'shutdown': `systemctl poweroff`,
+    'sleep': `systemctl suspend`
+}
 
-    if (!systemCom) {
+const handlerCom = (command) => {
+    const systemComWin = winCommands[command];
+    const systemComLin = linuxCommands[command]
+
+    if (!systemComWin || !linuxCommands) {
         return console.error('Ошибка, такой команды нет');
     };
 
-    exec(systemCom,{cwd: nircmdFolder}, error => {
+    if (systemComWin) {
+        exec(systemComWin,{cwd: nircmdFolder}, error => {
         if (error) {
             console.error('Ошибка выполнения', error.message);
         }
         console.log("Команда выполнилась")
     });
+    } else if (systemComLin) {
+        exec(systemComLin, error => {
+        if (error) {
+            console.error('Ошибка выполнения', error.message);
+        }
+        console.log("Команда выполнилась")
+    });
+    } else {
+        console.error('Ошибка, дял вашей ОС приложение еще не разработано')
+    }
+    
 };
 module.exports = { handlerCom };
