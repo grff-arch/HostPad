f8eebb0d933edb0abfd25cbeb3c4c0440d0d623e
grff-arch <andre.kiseleov2019@gmail.com>
2026-06-08 21:51:28 +0600

    Fixed for linux2

--- commands.js
+++ commands.js
@@ -18,15 +18,17 @@ const winCommands = {
 };
 
 const linuxCommands = {
-    'up': `wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+`,
+    'up': `wpctl set-volume -l 1.0 @DEFAULT_AUDIO_SINK@ 5%+`,
     'mute_on': `wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle`,
     'mute_off': `wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle`,
-    'down': `wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-0`,
-    'monitor_off': `busctl --user call org.gnome.Mutter.DisplayConfig /org/gnome/Mutter/DisplayConfig org.gnome.Mutter.DisplayConfig PowerSaveMode i 1`,
+    'down': `wpctl set-volume -l 1.0 @DEFAULT_AUDIO_SINK@ 5%-`,
+    'monitor_off': 'busctl --user set-property org.gnome.SettingsDaemon.Power /org/gnome/SettingsDaemon/Power org.gnome.SettingsDaemon.Power ScreenBlanking i 1',
+    'monitor_on': 'busctl --user set-property org.gnome.SettingsDaemon.Power /org/gnome/SettingsDaemon/Power org.gnome.SettingsDaemon.Power ScreenBlanking i 0',
+    'sleep': 'systemctl suspend',   
     'shutdown': `systemctl poweroff`,
     'sleep': `systemctl suspend`
 }
-
+    
 const handlerCom = (command) => {
     const systemComWin = winCommands[command];
     const systemComLin = linuxCommands[command]
@@ -54,4 +56,4 @@ const handlerCom = (command) => {
     }
     
 };
-module.exports = { handlerCom };
+module.exports = { handlerCom }; 
